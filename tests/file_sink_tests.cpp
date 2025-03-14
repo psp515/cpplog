90714b1e0df84dd0f736715d233e1eee200f2767
Łukasz Kolber <kolberu@gmail.com>
2025-03-14 12:36:17 +0100

    feat: first test

--- tests/file_sink_tests.cpp
+++ tests/file_sink_tests.cpp
@@ -2,7 +2,16 @@
 // Created by kolbe on 14.03.2025.
 //
 
+#include <iostream>
+#include <chrono>
+#include <iomanip>
+#include <ctime>
+#include <fstream>
+#include <ostream>
+#include <filesystem>
 #include <gtest/gtest.h>
+#include <cpplog/common/log.h>
+#include <cpplog/common/level.h>
 #include <cpplog/sinks/file_sink.h>
 
 using namespace std;
@@ -14,7 +23,35 @@ int Add(int a, int b) {
 }
 
 // Test case
-TEST(AdditionTest, PositiveNumbers) {
-  EXPECT_EQ(Add(2, 3), 5);  // Checks that 2 + 3 = 5
-  EXPECT_EQ(Add(7, 3), 10); // Checks that 7 + 3 = 10
+TEST(FileSinkTest, CreatesDataInFile) {
+	// Arrange
+  auto filename = "testFileSink.txt";
+  auto sink = make_unique<FileSink>(filename);
+
+  const filesystem::path dir(LOG_FOLDER);
+  const filesystem::path file(filename);
+  const filesystem::path full_path = dir / file;
+
+  const auto now = chrono::system_clock::now();
+  auto current_time = chrono::system_clock::to_time_t(now);
+  ostringstream oss;
+  oss << put_time(localtime(&current_time), "%Y-%m-%d %H:%M:%S");
+  const auto message = "Example message on " + oss.str();
+
+  auto line_written = false;
+
+  // Act
+  sink->log(Log(DEBUG, message));
+
+  ifstream written_file(full_path);
+  string line;
+  while (getline(written_file, line)) {
+    if (line.find(message)) {
+      line_written = true;
+      break;
+    }
+  }
+
+  // Assert
+  ASSERT_TRUE(line_written);
 }
