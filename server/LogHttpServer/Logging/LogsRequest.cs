5661ee3929e93e6e3f406517d57433dcbebc12b5
Łukasz Kolber <kolberu@gmail.com>
2025-03-14 13:34:33 +0100

    feat: http server for http sink

---
+++ server/LogHttpServer/Logging/LogsRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace LogHttpServer.Logging;
+
+public class LogsRequest
+{
+    public record LogDto
+    {
+        public string Message { get; set; } = string.Empty;
+    }
+
+    public List<LogDto> Logs { get; set; } = [];
+}
