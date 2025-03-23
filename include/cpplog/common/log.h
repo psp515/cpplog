8974df808723a97839730f39c90866bc53c97ece
Łukasz Kolber <69080157+psp515@users.noreply.github.com>
2025-03-23 20:26:14 +0100

    feat: log improvement

--- include/cpplog/common/log.h
+++ include/cpplog/common/log.h
@@ -5,6 +5,7 @@
 #ifndef LOG_H
 #define LOG_H
 
+#include <thread>
 #include <chrono>
 #include <cpplog/common/level.h>
 #include <source_location>
@@ -17,23 +18,32 @@ public:
 	Log(level level, const string& message, const std::source_location& location);
 	Log(level level, const string& message);
 
+	[[nodiscard]] std::thread::id get_thread() const {
+		return this->thread;
+	}
+
 	[[nodiscard]] string get_file_data() const {
 		return file_data;
 	}
+
 	[[nodiscard]] string get_function_data() const {
 		return function_data;
 	}
+
 	[[nodiscard]] string get_message() const {
 		return message;
 	}
+
 	[[nodiscard]] time_t get_timestamp() const {
 		return timestamp;
 	}
+	
 	[[nodiscard]] level get_level() const {
 		return level;
 	}
 
 private:
+	std::thread::id thread;
 	level level;
 	time_t timestamp;
 	string message;
--- src/common/log.cpp
+++ src/common/log.cpp
@@ -2,6 +2,7 @@
 // Created by psp515 on 23.03.2025.
 //
 
+#include <thread>
 #include <chrono>
 #include <source_location>
 #include <sstream>
@@ -12,9 +13,10 @@ using namespace cpplog::common;
 
 Log::Log(const enum level level, const string& message, const std::source_location& location) {
 
+	this->thread = std::this_thread::get_id();
 	this->level = level;
 	this->message = message;
-	this->timestamp = chrono::system_clock::to_time_t(chrono::system_clock::now());
+	this->timestamp = std::chrono::system_clock::to_time_t(chrono::system_clock::now());
 
 	ostringstream oss;
 	oss << location.file_name() << '(' << location.line() << ':' << location.column() << ")";
@@ -25,9 +27,10 @@ Log::Log(const enum level level, const string& message, const std::source_locati
 
 Log::Log(const enum level level, const string& message) {
 
+	this->thread = std::this_thread::get_id();
 	this->level = level;
 	this->message = message;
-	this->timestamp = chrono::system_clock::to_time_t(chrono::system_clock::now());
+	this->timestamp = std::chrono::system_clock::to_time_t(chrono::system_clock::now());
 	this->function_data = "Unknown";
 	this->file_data = "Unknown";
 }
