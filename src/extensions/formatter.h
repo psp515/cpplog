faac16947943c22566b349b84283e0ef6e57bc2b
Łukasz Kolber <kolberu@gmail.com>
2025-04-18 15:30:22 +0200

    feat(description)

--- src/extensions/formatter.h
+++ src/extensions/formatter.h
@@ -13,13 +13,25 @@ using namespace cpplog::common;
 
 namespace cpplog::extensions
 {
+    /// Class created for formatting final message to log.
     class Formatter
     {
     public:
+        /// Cannot create formatter without options.
         Formatter() = delete;
 
+        ///
+        /// Creates formatter object with options.
+        ///
+        /// @param options Options for formatting final log message.
         explicit Formatter(const LogEventOptions& options) : options(options) {}
 
+        ///
+        /// Method creates final message to log depending on options provided in constructor.
+        /// Can create message as text or json.
+        ///
+        /// @param event Log event.
+        /// @return Formatted message in json / text format depending on options.
         [[nodiscard]] string format(const LogEvent& event) const;
 
     private:
